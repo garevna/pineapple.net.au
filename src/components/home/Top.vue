6b9ff4dd8144ff807903159fd2fded3149692ad5
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2020-03-03 12:34:03 +0200

    review styles

--- src/components/home/Top.vue
+++ src/components/home/Top.vue
@@ -1,14 +1,14 @@
 <template>
   <v-row align="center" justify="center">
-    <v-col sm="12" md="7" lg="6" class="mx-auto">
+    <v-col sm="12" lg="6">
       <v-img src="@/assets/home/home-top-picture.png"></v-img>
     </v-col>
-    <v-col sm="12" md="5" lg="6" class="mx-auto">
+    <v-col sm="12" lg="6">
       <v-card flat class="transparent mx-auto">
         <v-card-title class="mx-auto">
           <h1 class="width-100 text-center text-lg-left">Australia's <span class="green--text">Fastest<br>Residential Internet</span><br>Connection</h1>
         </v-card-title>
-        <v-card-text class="mx-auto" style="max-width: 500px">
+        <v-card-text class="mx-sm-auto mx-lg-0" style="max-width: 500px">
           <h4 class="width-100 text-center text-lg-left">based on a comparison with speeds listed in the ACCC Broadband Performance Data Report</h4>
         </v-card-text>
       </v-card>
