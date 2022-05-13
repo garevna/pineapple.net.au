e1960084314df2418099fe60ee7b776051e441d8
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2022-05-13 07:07:41 +0300

    Main page award logo centered

--- src/components/home/Top.vue
+++ src/components/home/Top.vue
@@ -2,20 +2,20 @@
 <v-card flat class="top-container transparent mx-auto">
   <v-row align="center" justify="center">
     <v-col sm="12" md="6">
-        <v-card flat class="transparent mx-auto mt-lg-12">
-          <v-img :src="familyPicture" max-width="800" />
-        </v-card>
-      </v-col>
-      <v-col sm="12" md="6">
-        <v-card flat class="transparent mx-auto">
-          <v-card-title>
-            <h1 class="width-100 text-center text-md-left">Australia's <span class="green--text">Fastest<br>Residential Internet</span><br>Connection</h1>
-          </v-card-title>
-          <v-card-text class="width-100 text-center text-md-left mx-sm-auto mx-lg-0">
-            <h4 class="width-100 text-center text-md-left">{{ top.text }}</h4>
-            <v-img :src="awardLogo" max-width="180" class="mt-5" />
-          </v-card-text>
-        </v-card>
+      <v-card flat class="transparent mx-auto mt-lg-12">
+        <v-img :src="familyPicture" max-width="800" />
+      </v-card>
+    </v-col>
+    <v-col sm="12" md="6">
+      <v-card flat class="transparent mx-auto">
+        <v-card-title>
+          <h1 class="width-100 text-center text-md-left">Australia's <span class="green--text">Fastest<br>Residential Internet</span><br>Connection</h1>
+        </v-card-title>
+        <v-card-text class="width-100 text-center text-md-left mx-sm-auto mx-lg-0">
+          <h4 class="width-100 text-center text-md-left">{{ top.text }}</h4>
+          <v-img :src="awardLogo" max-width="180" class="mt-5 mx-auto" />
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-row>
 </v-card>
